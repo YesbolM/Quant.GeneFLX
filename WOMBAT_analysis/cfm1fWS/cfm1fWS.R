1b4ea84700dea26670d61d146bb0a731cc42dedb
Yesbol <48791487+YesbolM@users.noreply.github.com>
2025-11-24 20:15:16 -0600

    Add files via upload

---
+++ WOMBAT_analysis/cfm1fWS/cfm1fWS.R
@@ -0,0 +1,85 @@
+#rep1cfm fWS
+
+setwd("E:\\Lund\\Flies\\Yesbol\\Quant gen manuscript")
+
+#Import data
+data <- read.csv("data.csv")
+str(data)
+#Rename id column
+names(data)[1]<-"animal"
+#Make things that should be factors into factors
+for(x in c(1:5,12:14))data[,x]<-as.factor(data[,x])
+
+#Subset data
+rep1<-data[data$replicat=="rep1",]
+cfm1<-rep1[rep1$treatment=="CFM",] 
+#  WOMBAT does not allow any missing values, let's remove them
+cfm1<-na.omit(cfm1)
+cfm1f<-cfm1[cfm1$sex=="female",]
+str(cfm1f)
+#Select wing size for analysis
+#We need to include animal, WS, round, and round.sec
+cfm1fWS<-cfm1f[,c(1,7,13:14)]
+str(cfm1fWS)
+#Make round.sec into integers by taking factor codes
+cfm1fWS$round.sec <- as.numeric(cfm1fWS$round.sec)
+#Drop unused levels
+cfm1fWS<-droplevels(cfm1fWS) 
+str(cfm1fWS)
+#round: 4 levels
+length(unique(cfm1fWS$round.sec))
+#round.sec: 27 levels
+
+#Change ids into numeric values
+cfm1fWS$animal<-gsub("M","1",cfm1fWS$animal)
+cfm1fWS$animal<-gsub("F","2",cfm1fWS$animal)
+cfm1fWS$animal<-gsub("m","3",cfm1fWS$animal)
+cfm1fWS$animal<-gsub("f","4",cfm1fWS$animal)
+cfm1fWS$animal<-gsub("-","",cfm1fWS$animal)
+
+#Change name of round.sec to NSEC
+names(cfm1fWS)[4]<-"NSEC"
+
+#write data file
+setwd("E:\\Lund\\Flies\\Yesbol\\Quant gen manuscript\\Wombat analysis\\cfm1fWS")
+write.table(cfm1fWS, file="cfm1fWS_dat.dat", row.names = FALSE, quote = FALSE, col.names = FALSE)
+
+#Create pedigree.
+#M=1 (sire), F=2 (mother), m=3 (male offspring), f=4 (female offspring)
+setwd("E:\\Lund\\Flies\\Yesbol\\Quant gen manuscript")
+
+#Import data
+ped.cfm1 <- read.csv("rep1cfm.p.csv")
+head(ped.cfm1)
+str(ped.cfm1)
+#Change ids into numeric values
+ped.cfm1$id<-gsub("M","1",ped.cfm1$id)
+ped.cfm1$id<-gsub("F","2",ped.cfm1$id)
+ped.cfm1$id<-gsub("m","3",ped.cfm1$id)
+ped.cfm1$id<-gsub("f","4",ped.cfm1$id)
+ped.cfm1$id<-gsub("-","",ped.cfm1$id)
+ped.cfm1$FATHER<-gsub("M","1",ped.cfm1$FATHER)
+ped.cfm1$FATHER[is.na(ped.cfm1$FATHER)] <- 0
+ped.cfm1$MOTHER<-gsub("F","2",ped.cfm1$MOTHER)
+ped.cfm1$MOTHER[is.na(ped.cfm1$MOTHER)] <- 0
+
+#write pedigree file
+setwd("E:\\Lund\\Flies\\Yesbol\\Quant gen manuscript\\Wombat analysis\\cfm1fWS")
+write.table(ped.cfm1, file="cfm1fWS_ped.ped", row.names = FALSE, quote = FALSE, col.names = FALSE)
+
+#Get starting values for par file from MCMCglmm results
+setwd("E:\\Lund\\Flies\\Yesbol\\Quant gen manuscript\\univariate models")
+rep1CFMfWS <- readRDS("rep1CFM.fWS")
+library(MCMCglmm)
+posterior.mode(rep1CFMfWS$VCV)
+#animal    round.sec        units 
+#0.0020768812 0.0003171822 0.0007289045 
+
+# Testing significance of random effects
+#logL for model with animal: 1099.171
+#logL for model without animal: 1083.553
+q <- 2*(1099.171-1083.553)
+q
+#q=31.236
+pchisq(q, df=1, lower.tail = FALSE)
+#p=2.284893e-08
